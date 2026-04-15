cc9ff92db9298288510bfd93f8f3d16b6acf1e6c
Angel-1507 <daza85482@gmail.com>
2026-04-15 06:28:57 +0000

    Define la estructura base de los nodos del mapa. Incluye las propiedades fundamentales de cada habitación, como su ID, descripción, generación de ruido ambiental y la lógica booleana para permitir que el jugador se esconda.

---
+++ sala.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class Sala {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private boolean puedeEsconderse;
+    private boolean esconditeUsado;
+    private int ruidoBase;
+    private int frecuenciaRuido;
+    private boolean esSalidaFinal;
+    private ArrayList<Conexion> conexiones;
+
+    public Sala(int id, String nombre, String descripcion,
+            boolean escondite, int ruidoBase, int frecuenciaRuido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.puedeEsconderse = escondite;
+        this.esconditeUsado = false;
+        this.ruidoBase = ruidoBase;
+        this.frecuenciaRuido = frecuenciaRuido;
+        this.esSalidaFinal = false;
+        this.conexiones = new ArrayList<>();
+    }
+
+    public void agregarConexion(Sala destino, int ruido, String descripcion) {
+        this.conexiones.add(new Conexion(destino, ruido, descripcion));
+    }
+
+    public int getRuidoAmbiente(int turno) {
+        if (frecuenciaRuido == -1)
+            return 0;
+        if (frecuenciaRuido == 0)
+            return ruidoBase;
+        if (turno > 0 && turno % frecuenciaRuido == 0)
+            return ruidoBase;
+        return 0;
+    }
+
+    public boolean puedeEsconderseAqui() {
+        return puedeEsconderse && !esconditeUsado;
+    }
+
+    public void usarEscondite() {
+        this.esconditeUsado = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean tieneEscondite() {
+        return puedeEsconderse;
+    }
+
+    public boolean isEsconditeUsado() {
+        return esconditeUsado;
+    }
+
+    public boolean esSalidaFinal() {
+        return esSalidaFinal;
+    }
+
+    public void setSalidaFinal(boolean b) {
+        esSalidaFinal = b;
+    }
+
+    public ArrayList<Conexion> getConexiones() {
+        return conexiones;
+    }
+
+    public int getRuidoBase() {
+        return ruidoBase;
+    }
+
+    public int getFrecuenciaRuido() {
+        return frecuenciaRuido;
+    }
+}
